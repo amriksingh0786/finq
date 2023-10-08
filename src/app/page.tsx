cff3499f49d0ff2ddc82ff1ed7e64ac93f1c64ff
amrik <Amrik.Khalsa@cyncly.com>
2023-10-08 17:46:56 +0530

    corrected the text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
 export default function Home() {
   return (
-    <MaxWidthWrapper className='mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'> <div className='mx-auto '> kaala madarchod</div></MaxWidthWrapper>
+    <MaxWidthWrapper className='mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'> <div className='mx-auto '>hello world!!</div></MaxWidthWrapper>
   )
 }
